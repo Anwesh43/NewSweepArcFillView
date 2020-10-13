70945fbf739bec8ab590bdc04973d78da3bce4c7
Anwesh43 <anweshthecool0@gmail.com>
2020-10-14 03:05:07 +0530

    adding constants and libraries

--- newsweeparcfillview/src/main/java/com/anwesh/uiprojects/newsweeparcfillview/NewSweepArcFillView.kt
+++ newsweeparcfillview/src/main/java/com/anwesh/uiprojects/newsweeparcfillview/NewSweepArcFillView.kt
@@ -3,3 +3,30 @@ package com.anwesh.uiprojects.newsweeparcfillview
 /**
  * Created by anweshmishra on 14/10/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 0.02f
+val sizeFactor : Float = 2.9f
+val deg : Float = 90f
+val totDeg : Float = 360f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#4CAF50",
+        "#2196F3",
+        "#FF9800",
+        "#673AB7"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
