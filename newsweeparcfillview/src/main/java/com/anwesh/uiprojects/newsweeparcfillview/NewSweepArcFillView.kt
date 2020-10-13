222fd36543ccc26b563140aa7e10b2bc90acd1ca
Anwesh43 <anweshthecool0@gmail.com>
2020-10-14 03:06:24 +0530

    adding Int and Float extension methods

--- newsweeparcfillview/src/main/java/com/anwesh/uiprojects/newsweeparcfillview/NewSweepArcFillView.kt
+++ newsweeparcfillview/src/main/java/com/anwesh/uiprojects/newsweeparcfillview/NewSweepArcFillView.kt
@@ -30,3 +30,8 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.sin(this * Math.PI).toFloat()
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
