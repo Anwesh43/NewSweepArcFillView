c65808742cb7034ec0b956892629527d6048e50c
Anwesh43 <anweshthecool0@gmail.com>
2020-10-14 03:44:49 +0530

    addng fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkednewsweeparcfillview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkednewsweeparcfillview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkednewsweeparcfillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.newsweeparcfillview.NewSweepArcFillView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        NewSweepArcFillView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
