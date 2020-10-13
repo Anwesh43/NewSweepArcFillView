100294628a3df39c7c5e758310cb2850227997af
Anwesh43 <anweshthecool0@gmail.com>
2020-10-14 03:12:32 +0530

    adding Canvas extension methods

--- newsweeparcfillview/src/main/java/com/anwesh/uiprojects/newsweeparcfillview/NewSweepArcFillView.kt
+++ newsweeparcfillview/src/main/java/com/anwesh/uiprojects/newsweeparcfillview/NewSweepArcFillView.kt
@@ -13,7 +13,7 @@ import android.graphics.Color
 import android.graphics.RectF
 import android.content.Context
 
-val parts : Int = 3
+val parts : Int = 4
 val scGap : Float = 0.02f / parts
 val strokeFactor : Float = 0.02f
 val sizeFactor : Float = 2.9f
@@ -35,3 +35,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.sin(this * Math.PI).toFloat()
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawNewSweepArcFill(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(90f * j)
+        drawLine(0f, 0f, -size * sf1, 0f, paint)
+        restore()
+    }
+    for (j in 1..2) {
+        val sfj : Float = sf.divideScale(j, parts)
+        if (j == 1) {
+            paint.style = Paint.Style.STROKE
+        } else {
+            paint.style = Paint.Style.FILL
+        }
+        drawArc(RectF(-size, -size, size, size), -deg, (totDeg - deg) * sfj, j % 2 == 0, paint)
+    }
+    restore()
+}
+
+fun Canvas.drawNSAFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawNewSweepArcFill(scale, w, h, paint)
+}
